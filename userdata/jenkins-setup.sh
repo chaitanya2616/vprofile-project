de4ac5158f7d65af6fc43d3dc99c8a227826b675
chaitanya2616 <93932178+chaitanya2616@users.noreply.github.com>
2023-12-24 23:16:56 +0530

    Update jenkins-setup.sh
    update line number 6 by chaitanya

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -2,11 +2,17 @@
 sudo apt update
 sudo apt install openjdk-11-jdk -y
 sudo apt install maven -y
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+# updated jenkins.io-2023 
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
 sudo apt-get install jenkins -y
+# Start Jenkins
+sudo systemctl start jenkins
+
+# Enable Jenkins to run on Boot
+sudo systemctl enable jenkins
 ###
